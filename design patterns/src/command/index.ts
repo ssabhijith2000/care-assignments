6b4c82a8243ff119369e19caf3dda32bffffdf8e
abhijith <ssabhijith2000@gmail.com>
2022-10-23 20:58:38 +0530

    design patterns

---
+++ design patterns/src/command/index.ts
@@ -0,0 +1,8 @@
+import AddCustomerCommand from "./AddCustomerCommand";
+import Button from "./Button";
+import CustomerService from "./CustomerService.js";
+
+let service = new CustomerService()
+let command = new AddCustomerCommand(service)
+let button = new Button(command)
+button.click()
---
+++ design patterns/src/command/undo/index.ts
@@ -0,0 +1,4 @@
+class HtmlDocument {
+    content : string
+    
+}
