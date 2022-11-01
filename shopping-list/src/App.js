5c14a60b167e50e5ff91624a8239ec9eccceac13
abhijith <ssabhijith2000@gmail.com>
2022-11-01 10:43:32 +0530

    added shoppinglist project

---
+++ shopping-list/src/App.js
@@ -0,0 +1,12 @@
+import AddItemList from "./Shopping_List/AddItemForm";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>``
+      <AddItemList />
+    </div>
+  );
+}
+
+export default App;
